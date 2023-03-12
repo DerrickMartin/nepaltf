1344aba45c9cbda11a1c99d4d0a888e0c72589f2
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-03-12 03:07:02 +0000

    Updated goal componenet

--- src/components/DarkMode/About/team.js
+++ src/components/DarkMode/About/team.js
@@ -1,7 +1,18 @@
+import './teamStyles.css';
+
+
 export default function Team() {
-    return(
-    <>
-<p></p>
-</>
+    return (
+        <div className="team-section">
+            <h2>Team</h2>
+            <p> 
+            Our team is made up of individuals who are passionate about track and field
+            and who are committed to creating a supportive community for Nepali athletes.
+            </p>
+            <p>
+            Whether you're an aspiring athlete just starting out or a seasoned professional looking to connect with your fellow Nepali athletes, our website has something for everyone.
+            </p>
+
+        </div>
     )
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,15 +1,14 @@
 import Navbar from "../components/Navbar";
 import Footer from '../components/Footer';
-import Goal from "../components/DarkMode/About/goal";
 import Team from "../components/DarkMode/About/team";
-
+import GoalSection from "../components/DarkMode/About/goal";
 
 export default function About() {
     return (
         <>
         <Navbar />
         <>
-        <Goal />
+        <GoalSection />
         <Team />
         </>
         <Footer />
