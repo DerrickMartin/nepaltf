f53890c93931e7d73181d0a4344861ae0e6ea277
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-03-12 03:15:10 +0000

    Updated Home section

--- src/components/Home/homeSection.js
+++ src/components/Home/homeSection.js
@@ -1,9 +1,9 @@
 export default function HomeSelection() {
 return(
 <>
-<h1>Home</h1>
-<p>
-Welcome to our Nepali track and field website, a platform dedicated to bringing together athletes from all corners of the world. Our website is founded on the principle of unity and the belief that when like-minded individuals come together, anything is possible.
+<p style={{paddingTop: 20, paddingBottom: 20, textAlign: "center", justifyContent: "center", padding: 15}}>
+    <h3>Welcome to our Nepali track and field website,</h3>
+ a platform dedicated to bringing together athletes from all corners of the world. Our website is founded on the principle of unity and the belief that when like-minded individuals come together, anything is possible.
 We are committed to fostering a supportive and inclusive communitv that celebrates the achievements of Nepali athletes. Join us on this journey as we strive to elevate Nepali Track and Field to new heights.
 </p>
 </>
