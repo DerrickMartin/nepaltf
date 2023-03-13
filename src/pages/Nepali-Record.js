cc55bb8ff626186ff6f998a4c3eb479fd7894d0d
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-03-13 04:22:21 +0000

    Started  record list

--- src/pages/Nepali-Record.js
+++ src/pages/Nepali-Record.js
@@ -7,6 +7,24 @@ export default function Nepali_Records() {
         <>
         <Navbar />
         <h1>Nepali Records</h1>
+        <h5>Updated as of March 12, 2023</h5> 
+    
+        <div style={{width: '80%', flexDirection:'column', display: 'flex'}}>
+            <h4>Mens</h4>
+            <div className='RecordHeader'>
+                <p>100 meter</p>
+                <p>Name</p>
+                <p>Meet</p>
+            </div>
+        </div>
+        <div style={{width: '80%', flexDirection:'column', display: 'flex'}}>
+            <h4>Women</h4>
+            <div className='RecordHeader'>
+                <p>100 meter</p>
+                <p>Name</p>
+                <p>Meet</p>
+            </div>
+        </div>
         <Footer />
         </>
     )
