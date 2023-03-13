9bad7a8001a27a16038cc9764ada827cdf25711d
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-03-12 22:08:47 -0700

    Added translate button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,32 @@
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import HomeSelection from '../components/Home/homeSection';
-
+import { useEffect } from 'react';
 
 export default function Home() {
+const googleTranslateElementInit = () => {
+    new window.google.translate.TranslateElement(
+      {
+        pageLanguage: "en",
+        autoDisplay: false
+      },
+      "google_translate_element"
+    );
+  };
+  useEffect(() => {
+    var addScript = document.createElement("script");
+    addScript.setAttribute(
+      "src",
+      "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+    );
+    document.body.appendChild(addScript);
+    window.googleTranslateElementInit = googleTranslateElementInit;
     return (
         <>
         <Navbar />
+
         <div className="App">
+          <div id="google_translate_element"></div>
             <HomeSelection />
         </div>
         <Footer />
