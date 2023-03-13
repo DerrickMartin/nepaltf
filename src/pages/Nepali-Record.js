7af409cec6711a5efe14ee050f73d2638b682d97
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-03-13 18:56:48 +0000

    Added Verified Women Records

--- src/pages/Nepali-Record.js
+++ src/pages/Nepali-Record.js
@@ -119,8 +119,9 @@ export default function Nepali_Records() {
                 <p className='col_listing'>N/A</p>
                 <p className='col_listing'>Pokhara, Nepal</p>
             </div>
-
         </div>
+
+
         <div className="Record_List" >
             <h3 style={{width:'100', flexDirection: 'row', borderBottomStyle: 'solid', padding: 8, borderBottomColor: 'black',}}>Women</h3>
             <div className='RecordHeader'>
@@ -134,8 +135,8 @@ export default function Nepali_Records() {
 
             <div className='RecordListing'>
                 <p className='col_listing'>100 m</p>
-                <p className='col_listing'>10.71</p>
-                <p className='col_listing'>Ram Krishna Chaudhari</p>
+                <p className='col_listing'>12.19</p>
+                <p className='col_listing'>Devi Maya Paneru</p>
                 <p className='col_listing'>09/26/1999</p>
                 <p className='col_listing'>South Asian Games</p>
                 <p className='col_listing'>Kathmandu, Nepal</p>
@@ -143,11 +144,83 @@ export default function Nepali_Records() {
 
             <div className='RecordListing'>
                 <p className='col_listing'>200 m</p>
-                <p className='col_listing'>21.92</p>
-                <p className='col_listing'>Ram Krishna Chaudhari</p>
-                <p className='col_listing'>12/23/1995</p>
+                <p className='col_listing'>25.00</p>
+                <p className='col_listing'>Devi Maya Paneru</p>
+                <p className='col_listing'>12/28/1999</p>
                 <p className='col_listing'>South Asian Games</p>
-                <p className='col_listing'>Chennai, India</p>
+                <p className='col_listing'>Kathmandu, Nepal</p>
+            </div>
+
+            <div className='RecordListing'>
+                <p className='col_listing'>400 m</p>
+                <p className='col_listing'>59.90</p>
+                <p className='col_listing'>Pramila Rijal</p>
+                <p className='col_listing'>02/06/2010</p>
+                <p className='col_listing'>South Asian Games</p>
+                <p className='col_listing'>Dhaka, Bangladesh</p>
+            </div>
+
+            <div className='RecordListing'>
+                <p className='col_listing'>800 m</p>
+                <p className='col_listing'>2:11.05</p>
+                <p className='col_listing'>Raj Kumari Pandey</p>
+                <p className='col_listing'>10/23/1989</p>
+                <p className='col_listing'>South Asian Games</p>
+                <p className='col_listing'>Islamabad, Pakistan</p>
+            </div>
+
+            <div className='RecordListing'>
+                <p className='col_listing'>1500 m</p>
+                <p className='col_listing'>4:33.94</p>
+                <p className='col_listing'>Saraswati Bhattarai</p>
+                <p className='col_listing'>08/12/2016</p>
+                <p className='col_listing'>Olympic Games</p>
+                <p className='col_listing'>Rio de Janeiro, Brazil</p>
+            </div>
+
+            <div className='RecordListing'>
+                <p className='col_listing'>3000 m</p>
+                <p className='col_listing'>9:48.14</p>
+                <p className='col_listing'>Raj Kumari Pandey</p>
+                <p className='col_listing'>10/01/1990</p>
+                <p className='col_listing'>Asian Games</p>
+                <p className='col_listing'>Beijing, China</p>
+            </div>
+
+            <div className='RecordListing'>
+                <p className='col_listing'>5000 m</p>
+                <p className='col_listing'>16:49.98</p>
+                <p className='col_listing'>Kanchhi Maya Koju</p>
+                <p className='col_listing'>11/26/2010</p>
+                <p className='col_listing'>Asian Games</p>
+                <p className='col_listing'>Guangzhou, China</p>
+            </div>
+
+            <div className='RecordListing'>
+                <p className='col_listing'>10000 m</p>
+                <p className='col_listing'>35:03.43</p>
+                <p className='col_listing'>Kanchhi Maya Koju</p>
+                <p className='col_listing'>09/25/2006</p>
+                <p className='col_listing'>South Asian Games</p>
+                <p className='col_listing'>Colombo, Sri Lanka</p>
+            </div>
+
+            <div className='RecordListing'>
+                <p className='col_listing'>Marathon</p>
+                <p className='col_listing'>2:55:57</p>
+                <p className='col_listing'>Kanchhi Maya Koju</p>
+                <p className='col_listing'>09/24/2016</p>
+                <p className='col_listing'>N/A</p>
+                <p className='col_listing'>Kathmandu, Nepal</p>
+            </div>
+
+            <div className='RecordListing'>
+                <p className='col_listing'>10000 m</p>
+                <p className='col_listing'>35:03.43</p>
+                <p className='col_listing'>Kanchhi Maya Koju</p>
+                <p className='col_listing'>09/25/2006</p>
+                <p className='col_listing'>South Asian Games</p>
+                <p className='col_listing'>Colombo, Sri Lanka</p>
             </div>
         </div>
         <Footer />
