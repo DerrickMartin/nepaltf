66ceb988fe5031219307df81c12499e74e6b27d0
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-03-12 22:34:33 -0700

    Removed non working google translate buttton

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,9 @@
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import HomeSelection from '../components/Home/homeSection';
-import {useEffect } from 'react';
+
 
 export default function Home() {
-const googleTranslateElementInit = () => {
-    new window.google.translate.TranslateElement(
-      {
-        pageLanguage: "en",
-        autoDisplay: false
-      },
-      "google_translate_element"
-    );
-  };
-  useEffect(() => {
-    var addScript = document.createElement("script");
-    addScript.setAttribute(
-      "src",
-      "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
-    );
-    document.body.appendChild(addScript);
-    window.googleTranslateElementInit = googleTranslateElementInit;
-googleTranslateElementInit;
-  }, []);
     return (
         <>
         <Navbar />
