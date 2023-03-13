3931ccc42988fb883beb63e371514b4c93faafb7
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-03-12 22:32:09 -0700

    Started adding Nepali Records

--- src/pages/Nepali-Record.js
+++ src/pages/Nepali-Record.js
@@ -12,10 +12,32 @@ export default function Nepali_Records() {
         <div style={{width: '80%', flexDirection:'column', display: 'flex'}}>
             <h4>Mens</h4>
             <div className='RecordHeader'>
-                <p>100 meter</p>
-                <p>Name</p>
+                <p>Event</p>
+                <p>Record</p>
+                <p>Athlete</p>
+                <p>Date</p>
                 <p>Meet</p>
+                <p>Place</p>
             </div>
+
+            <div className='RecordListing'>
+              <p>100 m</p>
+              <p>10.71</p>
+              <p>Ram Krishna Chaudhari</p>
+              <p>09/26/1999</p>
+              <p>South Asian Games</p>
+              <p>Kathmandu, Nepal</p>
+            </div>
+
+            <div className='RecordListing'>
+              <p>200 m</p>
+              <p>21.92</p>
+              <p>Ram Krishna Chaudhari</p>
+              <p>12/23/1995</p>
+              <p>South Asian Games</p>
+              <p>Chennai, India</p>
+            </div>
+
         </div>
         <div style={{width: '80%', flexDirection:'column', display: 'flex'}}>
             <h4>Women</h4>
