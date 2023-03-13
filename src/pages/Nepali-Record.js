7189f521765ec423f0ff7a2cd3fb29146d0b7df6
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-03-13 16:54:47 +0000

    Added remaining mens records

--- src/pages/Nepali-Record.js
+++ src/pages/Nepali-Record.js
@@ -57,6 +57,69 @@ export default function Nepali_Records() {
                 <p className='col_listing'>Jakarata, Indonesia</p>
             </div>
 
+            <div className='RecordListing'>
+                <p className='col_listing'>1500 m</p>
+                <p className='col_listing'>3:46.63</p>
+                <p className='col_listing'>Gop Bahadur Adhikari</p>
+                <p className='col_listing'>12/22/1993</p>
+                <p className='col_listing'>South Asian Games</p>
+                <p className='col_listing'>Dhaka, Bangladesh</p>
+            </div>
+
+            <div className='RecordListing'>
+                <p className='col_listing'>3000 m</p>
+                <p className='col_listing'>8:23.59</p>
+                <p className='col_listing'>Gyan Bahadur Bohara</p>
+                <p className='col_listing'>10/15/1999</p>
+                <p className='col_listing'>N/A</p>
+                <p className='col_listing'>New Delhi, India</p>
+            </div>
+
+            <div className='RecordListing'>
+                <p className='col_listing'>5000 m</p>
+                <p className='col_listing'>14:04.89</p>
+                <p className='col_listing'>Rajendra Bahadur Bhandari</p>
+                <p className='col_listing'>08/25/2004</p>
+                <p className='col_listing'>Olympic Games</p>
+                <p className='col_listing'>Athens, Greece</p>
+            </div>
+
+            <div className='RecordListing'>
+                <p className='col_listing'>10000 m</p>
+                <p className='col_listing'>30:30.99</p>
+                <p className='col_listing'>Gopi Chandra Parki</p>
+                <p className='col_listing'>10/02/2014</p>
+                <p className='col_listing'>Asian Games</p>
+                <p className='col_listing'>Incheon, South Korea</p>
+            </div>
+
+            <div className='RecordListing'>
+                <p className='col_listing'>Marathon</p>
+                <p className='col_listing'>2:15:03</p>
+                <p className='col_listing'>Baikuntha Manadhar</p>
+                <p className='col_listing'>11/22/1987</p>
+                <p className='col_listing'>South Asian Games</p>
+                <p className='col_listing'>Calcutta, India</p>
+            </div>
+
+            <div className='RecordListing'>
+                <p className='col_listing'>3000 m steeplechase</p>
+                <p className='col_listing'>Rajendra Bahadur Bhandari</p>
+                <p className='col_listing'>Gop Bahadur Adhikari</p>
+                <p className='col_listing'>04/05/2004</p>
+                <p className='col_listing'>South Asian Games</p>
+                <p className='col_listing'>Islamabad, Pakistan</p>
+            </div>
+
+            <div className='RecordListing'>
+                <p className='col_listing'>Decathlon</p>
+                <p className='col_listing'>5355 pts(ht)</p>
+                <p className='col_listing'>Dambar Singh Kuwar</p>
+                <p className='col_listing'>09/27 - 09/28/1987</p>
+                <p className='col_listing'>N/A</p>
+                <p className='col_listing'>Pokhara, Nepal</p>
+            </div>
+
         </div>
         <div className="Record_List" >
             <h3 style={{width:'100', flexDirection: 'row', borderBottomStyle: 'solid', padding: 8, borderBottomColor: 'black',}}>Women</h3>
