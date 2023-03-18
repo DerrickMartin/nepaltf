322153ae6f9618a5e8147d3b3077e4cf165bfb67
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-03-18 20:26:04 +0000

    Updated Home Page

--- src/components/Home/homeSection.js
+++ src/components/Home/homeSection.js
@@ -1,12 +1,19 @@
 import NepalHero from '../../assets/nepal_home.jpg';
+import './HomeSelectionStyles.css';
 
 export default function HomeSelection() {
 return(
 <>
-<img src={NepalHero} style={{width: '94%'}} alt="Home Page Nepal" />
+<div className='HeroSection'>
+    <img src={NepalHero} className='HeroImage' alt="Home Page Nepal" />
+    <>
+        <h3 className='HeroText'>Welcome to our Nepali track and field website</h3>
+
+    </>
+</div>
 <p style={{paddingTop: 20, paddingBottom: 20, textAlign: "center", justifyContent: "center", padding: 15}}>
-    <h3>Welcome to our Nepali track and field website,</h3>
- a platform dedicated to bringing together athletes from all corners of the world. Our website is founded on the principle of unity and the belief that when like-minded individuals come together, anything is possible.
+    
+ A platform dedicated to bringing together athletes from all corners of the world. Our website is founded on the principle of unity and the belief that when like-minded individuals come together, anything is possible.
 We are committed to fostering a supportive and inclusive community that celebrates the achievements of Nepali athletes. Join us on this journey as we strive to elevate Nepali Track and Field to new heights.
 </p>
 </>
