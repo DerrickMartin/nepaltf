631bf1508bb91cc6997e841a09e37463c5110dcc
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-05-16 22:05:27 +0000

    Turned mp4 video into a gif

--- src/components/Home/homeSection.js
+++ src/components/Home/homeSection.js
@@ -1,16 +1,15 @@
 
 import './HomeSelectionStyles.css';
 
-import videoFile from '../../assets/HomePage.mp4';
 
 export default function HomeSelection() {
 return(
 <>
 <div className='HeroSection'>
-    <video muted loop autoplay className='HeroImage'>
-        <source src={videoFile} type="video/mp4" />
-    Your browser does not support the video tag.
-    </video>
+    <div className='HeroImage'>
+<iframe src="https://giphy.com/embed/AntXHmBCQjxT9W0EkS" width="480" height="270" title='Nepal' className="giphy-embed" allowFullScreen></iframe>
+</div>
+
     <>
         <h3 className='HeroText'>Welcome to our Nepali track and field website</h3>
 
