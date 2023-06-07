1e37cc51b13b995edec0039bcf924f043e61b21c
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-06-07 18:15:22 +0000

    Updated Total Visit adding gs

---
+++ src/google-analytics.js
@@ -0,0 +1,8 @@
+// google-analytics.js
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  window.dataLayer.push(arguments);
+}
+gtag('js', new Date());
+
+gtag('config', 'G-0NC8MKM0QW');
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import  './google-analytics';
+
+
 import ReactGA from 'react-ga';
 
 // Initialize Google Analytics with your measurement ID
