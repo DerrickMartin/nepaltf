dbc90a3364a5177f18a0072bb70700419e40265a
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-06-07 17:52:08 +0000

    Updated SiteVisit js

--- src/components/SiteVisit.js
+++ src/components/SiteVisit.js
@@ -5,17 +5,33 @@ function SiteVisit() {
   const [visitCount, setVisitCount] = useState(0);
 
   useEffect(() => {
-    // Increment the visit count
-    setVisitCount((prevCount) => prevCount + 1);
+    // Update the visit count when the component mounts
+    setVisitCount(getVisitCountFromLocalStorage());
 
     // Send a pageview event to Google Analytics
     ReactGA.pageview(window.location.pathname);
   }, []);
 
+  const incrementCount = () => {
+    const newCount = visitCount + 1;
+    setVisitCount(newCount);
+    saveVisitCountToLocalStorage(newCount);
+  };
+
+  const saveVisitCountToLocalStorage = (count) => {
+    localStorage.setItem('visitCount', count);
+  };
+
+  const getVisitCountFromLocalStorage = () => {
+    const count = localStorage.getItem('visitCount');
+    return count ? parseInt(count, 10) : 0;
+  };
+
   return (
     <div>
       <h2>Total Site Visits</h2>
       <p>Count: {visitCount}</p>
+      <button onClick={incrementCount}>Increment Count</button>
     </div>
   );
 }
